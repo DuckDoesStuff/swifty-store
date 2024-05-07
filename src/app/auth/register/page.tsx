f9063099cebaea330207ddeab59d0a71e9e61cd2
Phuong Banh <m.phuong219@gmail.com>
2024-05-07 20:08:02 +0700

    create Sign in and Register Page

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,12 @@
+import RegisterForm from "@/components/RegisterForm";
+const RegisterPage: React.FC = () => {
+    return (
+            <div className={"flex justify-center items-center min-h-screen"}>
+                <RegisterForm/>
+            </div>
+    )
+
+       
+
+}
+export default RegisterForm;
---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,9 @@
+import SiginForm from "@/components/SigninForm";
+
+const SigninPage = () =>{
+    return (
+        <div className={"flex justify-center items-center min-h-screen"}><SiginForm/></div>
+    
+    )
+}
+export default SigninPage
