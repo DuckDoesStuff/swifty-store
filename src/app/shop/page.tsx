83f1fd7ad8fd2ef6356728749610f807322658ba
nhungnguyen1503 <nhung1899@gmail.com>
2024-05-11 17:32:41 +0700

    feature: trang ban hang

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,8 +1,30 @@
 import ShopInfo from "@/components/shop/ShopInfo";
+import Product from "@/components/ProductItem";
 export default function shop() {
+    const shop = {
+        id: 1,
+        displayName: "DuckDoesStuff",
+        createdAt: "11/05/2024",
+        products: [
+            { id: 1, img:"https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", name: "Product 1", price: "$100" },
+            { id: 2, img:"https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", name: "Product 2", price: "$200" },
+            { id: 3, img:"https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", name: "Product 3", price: "$300" }
+        ],
+        solds: "102",
+        logo: "https://cafefcdn.com/thumb_w/640/203337114487263232/2022/5/15/photo-1-16525767855142091331258.jpg"
+    };
+            
     return (
-        <div>
-            <ShopInfo/>
+        <div className={"flex flex-col gap-10 px-10"}>
+            <ShopInfo displayName={shop.displayName} createdAt={shop.createdAt} products={shop.products.length.toString()} solds={shop.solds} logo={shop.logo}/>
+            <div className="flex justify-between items-center">
+                <p className="text-2xl font-bold">Shop Products</p>
+            </div>
+            <div className="grid lg:grid-cols-5 gap-10 sm:grid-cols-3">
+                {shop.products.map(product => (
+                    <Product imageSource={product.img} productName={product.name} price={product.price}/>
+                    ))}
+            </div>
         </div>
     )
 }
--- src/components/shop/ShopInfo.tsx
+++ src/components/shop/ShopInfo.tsx
@@ -1,42 +1,36 @@
-import { CiSquarePlus } from "react-icons/ci";
-export default function ShopInfo() {
+interface ShopProps {
+    displayName:string;
+    createdAt:string;
+    products:string;
+    solds:string;
+    logo:string;
+  }
+export default function ShopInfo({ displayName, createdAt, products, solds, logo }: ShopProps) {
     return(
         <div className="flex items-center gap-20 justify-center">
             <div className="flex justify-left items-center gap-4" >
                 <div className="h-20 w-20 overflow-hidden rounded-full">
                     <img
                         alt=""
-                        src="https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        src={logo}
                         className="h-full w-full object-cover"
                     />
                 </div>
                 <div className="flex items-center">
                     <div className="flex flex-col gap-1">
-                        <p className="text-lg font-semibold"> Tên shop </p>
-                        <button className="flex items-center gap-1 rounded-md px-5 py-2.5 text-sm font-medium text-gray-500 border border-gray-300"> <CiSquarePlus /> Follow </button>
+                        <p className="text-lg font-semibold"> {displayName} </p>
+                        <p className="flex items-center gap-1 text-sm font-medium text-gray-500"> Joined at {createdAt} </p>
                     </div>
                 </div>
             </div>
             <div className="flex gap-20">
-                <div>
-                    <div className="flex gap-2">
-                        <p className="font-semibold text-gray-800"> Product:</p>
-                        <p className="text-gray-500"> 300 </p>
-                    </div>
-                    <div className="flex gap-2">
-                        <p className="font-semibold text-gray-800"> Follower:</p>
-                        <p className="text-gray-500"> 300 </p>
-                    </div>
+                <div className="flex gap-2">
+                    <p className="font-semibold text-gray-800"> Product:</p>
+                    <p className="text-gray-500"> {products} </p>
                 </div>
-                <div>
-                    <div className="flex gap-2">
-                        <p className="font-semibold text-gray-800"> Rating:</p>
-                        <p className="text-gray-500"> 300 </p>
-                    </div>
-                    <div className="flex gap-2">
-                        <p className="font-semibold text-gray-800"> Following:</p>
-                        <p className="text-gray-500"> 300 </p>
-                    </div>
+                <div className="flex gap-2">
+                    <p className="font-semibold text-gray-800"> Sold:</p>
+                    <p className="text-gray-500"> {solds} </p>
                 </div>
             </div>
         </div>
